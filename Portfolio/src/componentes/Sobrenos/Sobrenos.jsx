8395b1319e20bab6df7e8509ea01cf4deb464501
yorkt1 <guilherme_oliveira18@estudante.sesisenai.org.br>
2024-11-05 16:57:08 -0300

    Iniciação da tela sobrenos

---
+++ Portfolio/src/componentes/Sobrenos/Sobrenos.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Sobrenos.css' 
+
+ function Sobrenos() {
+   return(
+
+      <div className="conteiner-geral">a
+    AA
+    
+    </div>
+   )
+ }
+
+
+ export default Sobrenos;
