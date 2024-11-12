adcf7552536252a287eb0062c77da56b131376e2
yorkt1 <guilherme_oliveira18@estudante.sesisenai.org.br>
2024-11-12 14:19:39 -0300

    preparação para o efeito nav

--- src/componentes/Home/Home.jsx
+++ src/componentes/Home/Home.jsx
@@ -2,55 +2,15 @@ import React, { useState } from 'react';
 import './Home.css'; // Supondo que o CSS seja colocado em um arquivo separado.
 
 const Home = () => {
-  const [activeIndex, setActiveIndex] = useState(null);
 
-  const handleClick = (index) => {
-    setActiveIndex(index);
-  };
 
   return (
     <div>
-      <nav>
-        <ul>
-          <li 
-            className={activeIndex === 0 ? 'active' : ''}
-            onClick={() => handleClick(0)}
-          >
-            <a href="Salfia.co">Home</a>
-          </li>
-          <li 
-            className={activeIndex === 1 ? 'active' : ''}
-            onClick={() => handleClick(1)}
-          >
-            About me
-          </li>
-          <li 
-            className={activeIndex === 2 ? 'active' : ''}
-            onClick={() => handleClick(2)}
-          >
-            Portfolio
-          </li>
-          <li 
-            className={activeIndex === 3 ? 'active' : ''}
-            onClick={() => handleClick(3)}
-          >
-            Blog
-          </li>
-          <li 
-            className={activeIndex === 4 ? 'active' : ''}
-            onClick={() => handleClick(4)}
-          >
-            Contact me
-          </li>
-          <li className="slide"></li>
-        </ul>
-      </nav>
-
       <section>
         <div className="title">
-          <span>Jack Salfia</span>
+          <span>Guilherme Oliveira</span>
           <span>
-            Graphic Designer and 
+            Desenvolvedor de sistemas
             <a target="_blank" href="salfia.com">Web Developer</a>
           </span>
         </div>
--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import "./NavbarStyles.css"
+import logo from '../Navbar/logo-fac.png'
 import { Component } from "react";
 
 class Navbar extends Component {
@@ -12,27 +13,48 @@ class Navbar extends Component {
 
         
         <>
-       <nav>
-        <a href="index.html">Guilherme</a>
+        <nav>
+            <div className="logo">
+                <img  className='img-logo'src={logo} alt="Logo" />
+                <a href="index.html">
+                    Guilherme
+                    <span className="oliveira"> Oliveira</span>
+                </a>
+            </div>
+            <a href="index.html">Guilherme</a>
 
-
-<div>
-    <ul id="navbar" className={this.state.clicked ? "#navbar  active" : "#navbar"}>
-        <li className="active"><a className="a" href="index.html">Home</a></li>
-        <li><a href="index.html">Sobre</a></li>
-        <li><a href="index.html">Habilidades</a></li>
-        <li><a href="index.html">Projeto</a></li>
-        <li><a href="index.html">Me contrate </a></li>
-       </ul>
-       </div>
-       <div id="mobile" onClick={this.handclick}>
-        <i id="bar"
-        className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-       </div>
-       </nav>
-       
-        </> 
-
+            <div>
+                <ul
+                    id="navbar"
+                    className={this.state.clicked ? "#navbar active" : "#navbar"}
+                >
+                    <li className="active">
+                        <a className="a" href="index.html">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="index.html">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="index.html">Habilidades</a>
+                    </li>
+                    <li>
+                        <a href="index.html">Projeto</a>
+                    </li>
+                    <li>
+                        <a href="index.html">Me contrate </a>
+                    </li>
+                </ul>
+            </div>
+            <div id="mobile" onClick={this.handclick}>
+                <i
+                    id="bar"
+                    className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}
+                ></i>
+            </div>
+        </nav>
+    </>
         
     )
     
