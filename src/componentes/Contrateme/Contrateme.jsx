cba71de4e72fa634f3a253839b1df57cfaffb68d
yorkt1 <guilherme_oliveira18@estudante.sesisenai.org.br>
2024-11-12 21:19:29 -0300

    Efeito button contrate-me

--- src/componentes/Contrateme/Contrateme.jsx
+++ src/componentes/Contrateme/Contrateme.jsx
@@ -38,30 +38,55 @@ const Contrateme = () => {
 
   return (
     <div className="contratameprincipal">
-      <form onSubmit={handleSubmit}>
-        <label>Name</label>
-        <input 
-          type="text"
-          placeholder="Nome"
-          value={name}
-          onChange={(e) => setName(e.target.value)} 
-        />
-        <label>Email</label>
-        <input 
-          type="email" 
-          placeholder="E-mail"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)} 
-        />
-        <label>Message</label>
-        <textarea
-          cols="30"
-          rows="10"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
-        <button type="submit">Enviar email</button>
-      </form>
+      <div className="sessao">
+        <h2>Contato</h2>
+        <form onSubmit={handleSubmit}>
+          <label>Name</label>
+          <input 
+            type="text"
+            placeholder="Nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)} 
+          />
+          <label>Email</label>
+          <input 
+            type="email" 
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+          <label>Message</label>
+          <textarea
+            cols="30"
+            rows="10"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          
+          {/* Botão estilizado com estrelas */}
+          <button className="styled-button" type="submit">
+            Enviar Email
+            <div className="star-1">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53"><path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0"></path></svg>
+            </div>
+            <div className="star-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53"><path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0"></path></svg>
+            </div>
+            <div className="star-3">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53"><path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0"></path></svg>
+            </div>
+            <div className="star-4">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53"><path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0"></path></svg>
+            </div>
+            <div className="star-5">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53"><path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0"></path></svg>
+            </div>
+            <div className="star-6">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53"><path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0"></path></svg>
+            </div>
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
