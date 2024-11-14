e844f66b37f9293f530b7e5e03139f219913bbed
yorkt1 <guilherme_oliveira18@estudante.sesisenai.org.br>
2024-11-14 17:21:09 -0300

    algumas atualizações para o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Habilidades from './componentes/habilidades/Habilidades.jsx'
 import Home from './componentes/Home/Home.jsx';
 import Contrateme from './componentes/Contrateme/Contrateme.jsx'
 import './App.css'
+import Projetos from './componentes/Projetos/Projetos.jsx';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Home />
       <Habilidades />
       <Sobrenos />
+      <Projetos/>
       <Contrateme/>
     
     </>
---
+++ src/componentes/Projetos/Projetos.jsx
@@ -0,0 +1,11 @@
+function Projetos(){
+return(
+
+<>a</>
+
+)
+
+}
+
+
+export default Projetos
