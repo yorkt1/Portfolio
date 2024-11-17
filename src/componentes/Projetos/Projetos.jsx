052392d4aad91f954040197790e40980574fed70
yorkt1 <guilherme_oliveira18@estudante.sesisenai.org.br>
2024-11-16 22:31:48 -0300

    atualização de classes

--- src/componentes/Projetos/Projetos.jsx
+++ src/componentes/Projetos/Projetos.jsx
@@ -5,147 +5,98 @@ function Projetos(){
 
 return(
 
-<div class="grid--container">
+<div className="grid--container">
 
 
-<div class="grid--cell">
-  <article class="grid--item">
+<div className="grid--cell">
+  <article className="grid--item">
     
-    <div class="preview--container">
-      <a href="#" class="preview-image--container">
-        <div class="preview-image"></div>
+    <div className="preview--container">
+      <a href="#" className="preview-image--container">
+        <div className="preview-image"></div>
       </a>
      
     </div>
     
-    <div class="content--container">
-      <div class="title--container">
-        <a class="title--text" href="#">Projeto 1</a>
+    <div className="content--container">
+      <div className="title--container">
+        <a className="title--text" href="#">Projeto 1</a>
       </div>
       
-      <div class="tags--overflow-container">
-        <ul class="tags--container">
-          <li><a href="#" class="tag">Delect a Distinctio</a></li>
-          <li><a href="#" class="tag">Facere</a></li>
-          <li><a href="#" class="tag">Morbi Leo</a></li>
-          <li><a href="#" class="tag">Eris Culpa</a></li>
-          <li><a href="#" class="tag">Facere</a></li>
-          <li><a href="#" class="tag">Quisus</a></li>
-          <li><a href="#" class="tag ellipsis"><i class="far fa-ellipsis-h"></i></a></li>
+      <div className="tags--overflow-container">
+        <ul className="tags--container">
+          <li><a href="#" className="tag">Delect a Distinctio</a></li>
+          <li><a href="#" className="tag">Facere</a></li>
+          <li><a href="#" className="tag">Morbi Leo</a></li>
+          <li><a href="#" className="tag">Eris Culpa</a></li>
+          <li><a href="#" className="tag">Facere</a></li>
+          <li><a href="#" className="tag">Quisus</a></li>
+          <li><a href="#" className="tag ellipsis"><i className="far fa-ellipsis-h"></i></a></li>
         </ul>
       </div>
       
-      <div class="hover--options">
-        <a href="#" class="Repositório button">
-          <span class="icon-title"><i class="fad fa-books"></i> Repositório</span>
-          <span class="new-tab"><i class="fas fa-arrow-circle-right"></i></span>
+      <div className="hover--options">
+        <a href="#" className="Repositório button">
+          <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
+          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
         </a>
         
-        <a href="#" class="Deploy
+        <a href="#" className="Deploy
          button">
-          <span class="icon-title"><i class="far fa-image"></i> Deploy
+          <span className="icon-title"><i className="far fa-image"></i> Deploy
         </span>
-          <span class="new-tab"><i class="fas fa-arrow-circle-right"></i></span>
+          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
         </a>
         
-        <a href="#" class="follow button"><i class="fas fa-plus"></i></a>
+        <a href="#" className="follow button"><i className="fas fa-plus"></i></a>
       </div>
     </div>
     
   </article>
 </div>
 
-<div class="grid--cell">
-  <article class="grid--item">
+<div className="grid--cell">
+  <article className="grid--item">
     
-    <div class="preview--container">
-      <a href="#" class="preview-image--container">
-        <div class="preview-image"></div>
+    <div className="preview--container">
+      <a href="#" className="preview-image--container">
+        <div className="preview-image"></div>
       </a>
       
      
     </div>
     
-    <div class="content--container">
-      <div class="title--container">
-        <a class="title--text" href="#">Projeto 2</a>
+    <div className="content--container">
+      <div className="title--container">
+        <a className="title--text" href="#">Projeto 2</a>
       </div>
       
-      <div class="tags--overflow-container">
-        <ul class="tags--container">
-          <li><a href="#" class="tag">Volutpat</a></li>
-          <li><a href="#" class="tag">Scelerisque</a></li>
-          <li><a href="#" class="tag">Turpis Galo</a></li>
-          <li><a href="#" class="tag">Quisus</a></li>
-          <li><a href="#" class="tag">At Tempo</a></li>
-          <li><a href="#" class="tag">Distincto a Tet</a></li>
-          <li><a href="#" class="tag ellipsis"><i class="far fa-ellipsis-h"></i></a></li>
+      <div className="tags--overflow-container">
+        <ul className="tags--container">
+          <li><a href="#" className="tag">Volutpat</a></li>
+          <li><a href="#" className="tag">Scelerisque</a></li>
+          <li><a href="#" className="tag">Turpis Galo</a></li>
+          <li><a href="#" className="tag">Quisus</a></li>
+          <li><a href="#" className="tag">At Tempo</a></li>
+          <li><a href="#" className="tag">Distincto a Tet</a></li>
+          <li><a href="#" className="tag ellipsis"><i className="far fa-ellipsis-h"></i></a></li>
         </ul>
       </div>
       
-      <div class="hover--options">
-        <a href="#" class="Repositório button">
-          <span class="icon-title"><i class="fad fa-books"></i> Repositório</span>
-          <span class="new-tab"><i class="fas fa-arrow-circle-right"></i></span>
+      <div className="hover--options">
+        <a href="#" className="Repositório button">
+          <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
+          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
         </a>
         
-        <a href="#" class="Deploy
+        <a href="#" className="Deploy
          button">
-          <span class="icon-title"><i class="far fa-image"></i> Deploy
+          <span className="icon-title"><i className="far fa-image"></i> Deploy
         </span>
-          <span class="new-tab"><i class="fas fa-arrow-circle-right"></i></span>
+          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
         </a>
         
-        <a href="#" class="follow button"><i class="fas fa-plus"></i></a>
-      </div>
-    </div>
-    
-  </article>
-</div>
-
-
-<div class="grid--cell">
-  <article class="grid--item">
-    
-    <div class="preview--container">
-      <a href="#" class="preview-image--container">
-        <div class="preview-image"></div>
-      </a>
-      
-     
-    </div>
-    
-    <div class="content--container">
-      <div class="title--container">
-        <a class="title--text" href="#">Projeto 3</a>
-      </div>
-      
-      <div class="tags--overflow-container">
-        <ul class="tags--container">
-          <li><a href="#" class="tag">Gravidas</a></li>
-          <li><a href="#" class="tag">Dipisci Triste</a></li>
-          <li><a href="#" class="tag">Odio Aenean</a></li>
-          <li><a href="#" class="tag">Urna</a></li>
-          <li><a href="#" class="tag">Lista e Fortna</a></li>
-          <li><a href="#" class="tag">Vuisi</a></li>
-          <li><a href="#" class="tag ellipsis"><i class="far fa-ellipsis-h"></i></a></li>
-        </ul>
-      </div>
-      
-      <div class="hover--options">
-        <a href="#" class="Repositório button">
-          <span class="icon-title"><i class="fad fa-books"></i> Repositório</span>
-          <span class="new-tab"><i class="fas fa-arrow-circle-right"></i></span>
-        </a>
-        
-        <a href="#" class="Deploy
-         button">
-          <span class="icon-title"><i class="far fa-image"></i> Deploy
-        </span>
-          <span class="new-tab"><i class="fas fa-arrow-circle-right"></i></span>
-        </a>
-        
-        <a href="#" class="follow button"><i class="fas fa-plus"></i></a>
+        <a href="#" className="follow button"><i className="fas fa-plus"></i></a>
       </div>
     </div>
     
@@ -153,47 +104,96 @@ return(
 </div>
 
 
-<div class="grid--cell">
-  <article class="grid--item">
+<div className="grid--cell">
+  <article className="grid--item">
     
-    <div class="preview--container">
-      <a href="#" class="preview-image--container">
-        <div class="preview-image"></div>
+    <div className="preview--container">
+      <a href="#" className="preview-image--container">
+        <div className="preview-image"></div>
       </a>
       
      
     </div>
     
-    <div class="content--container">
-      <div class="title--container">
-        <a class="title--text" href="#">Projeto 4</a>
+    <div className="content--container">
+      <div className="title--container">
+        <a className="title--text" href="#">Projeto 3</a>
       </div>
       
-      <div class="tags--overflow-container">
-        <ul class="tags--container">
-          <li><a href="#" class="tag">Elementum</a></li>
-          <li><a href="#" class="tag">Praesent Semper</a></li>
-          <li><a href="#" class="tag">Gravida</a></li>
-          <li><a href="#" class="tag">Libero</a></li>
-          <li><a href="#" class="tag">Hesus</a></li>
-          <li><a href="#" class="tag ellipsis"><i class="far fa-ellipsis-h"></i></a></li>
+      <div className="tags--overflow-container">
+        <ul className="tags--container">
+          <li><a href="#" className="tag">Gravidas</a></li>
+          <li><a href="#" className="tag">Dipisci Triste</a></li>
+          <li><a href="#" className="tag">Odio Aenean</a></li>
+          <li><a href="#" className="tag">Urna</a></li>
+          <li><a href="#" className="tag">Lista e Fortna</a></li>
+          <li><a href="#" className="tag">Vuisi</a></li>
+          <li><a href="#" className="tag ellipsis"><i className="far fa-ellipsis-h"></i></a></li>
         </ul>
       </div>
       
-      <div class="hover--options">
-        <a href="#" class="Repositório button">
-          <span class="icon-title"><i class="fad fa-books"></i> Repositório</span>
-          <span class="new-tab"><i class="fas fa-arrow-circle-right"></i></span>
+      <div className="hover--options">
+        <a href="#" className="Repositório button">
+          <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
+          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
         </a>
         
-        <a href="#" class="Deploy
+        <a href="#" className="Deploy
          button">
-          <span class="icon-title"><i class="far fa-image"></i> Deploy
+          <span className="icon-title"><i className="far fa-image"></i> Deploy
         </span>
-          <span class="new-tab"><i class="fas fa-arrow-circle-right"></i></span>
+          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
         </a>
         
-        <a href="#" class="follow button"><i class="fas fa-plus"></i></a>
+        <a href="#" className="follow button"><i className="fas fa-plus"></i></a>
+      </div>
+    </div>
+    
+  </article>
+</div>
+
+
+<div className="grid--cell">
+  <article className="grid--item">
+    
+    <div className="preview--container">
+      <a href="#" className="preview-image--container">
+        <div className="preview-image"></div>
+      </a>
+      
+     
+    </div>
+    
+    <div className="content--container">
+      <div className="title--container">
+        <a className="title--text" href="#">Projeto 4</a>
+      </div>
+      
+      <div className="tags--overflow-container">
+        <ul className="tags--container">
+          <li><a href="#" className="tag">Elementum</a></li>
+          <li><a href="#" className="tag">Praesent Semper</a></li>
+          <li><a href="#" className="tag">Gravida</a></li>
+          <li><a href="#" className="tag">Libero</a></li>
+          <li><a href="#" className="tag">Hesus</a></li>
+          <li><a href="#" className="tag ellipsis"><i className="far fa-ellipsis-h"></i></a></li>
+        </ul>
+      </div>
+      
+      <div className="hover--options">
+        <a href="#" className="Repositório button">
+          <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
+          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
+        </a>
+        
+        <a href="#" className="Deploy
+         button">
+          <span className="icon-title"><i className="far fa-image"></i> Deploy
+        </span>
+          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
+        </a>
+        
+        <a href="#" className="follow button"><i className="fas fa-plus"></i></a>
       </div>
     </div>
     
