d5890d2e111968a0c3b43ae53847186d0a4cd46a
yorkt1 <guilherme_oliveira18@estudante.sesisenai.org.br>
2024-11-18 21:33:40 -0300

    atualização de cards

--- src/componentes/Projetos/Projetos.jsx
+++ src/componentes/Projetos/Projetos.jsx
@@ -27,43 +27,39 @@ return(
   <article className="grid--item">
     
     <div className="preview--container">
-      <a href="#" className="preview-image--container">
-        <div className="preview-image"></div>
+      <a href="https://farmacia-atividade.vercel.app/#" className="preview-imageProjetoUm--container">
+        <div className="preview-imageProjetoUm"></div>
       </a>
      
     </div>
     
     <div className="content--container hiddenCards">
       <div className="title--container">
-        <a className="title--text" href="#">Projeto 1</a>
+        <a className="title--text" href="https://farmacia-atividade.vercel.app/#">Projeto 1</a>
       </div>
       
-      <div className="tags--overflow-container">
+      <div className="tags--overflow-container" >
         <ul className="tags--container">
-          <li><a href="#" className="tag">Delect a Distinctio</a></li>
-          <li><a href="#" className="tag">Facere</a></li>
-          <li><a href="#" className="tag">Morbi Leo</a></li>
-          <li><a href="#" className="tag">Eris Culpa</a></li>
-          <li><a href="#" className="tag">Facere</a></li>
-          <li><a href="#" className="tag">auisus</a></li>
-          <li><a href="#" className="tag ellipsis"><i className="far fa-ellipsis-h"></i></a></li>
+          <li><a href="https://farmacia-atividade.vercel.app/#" className="tag">Delect a Distinctio</a></li>
+       
+          <li><a href="https://farmacia-atividade.vercel.app/#" className="tag ellipsis"><i className="far fa-ellipsis-h"></i></a></li>
         </ul>
       </div>
       
       <div className="hover--options">
-        <a href="#" className="Repositório button">
+        <a href="https://github.com/yorkt1/Farmacia-Atividade" className="Repositório button">
           <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
           <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
         </a>
         
-        <a href="#" className="Deploy
+        <a href="https://farmacia-atividade.vercel.app/#" className="Deploy
          button">
           <span className="icon-title"><i className="far fa-image"></i> Deploy
         </span>
           <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
         </a>
         
-        <a href="#" className="follow button"><i className="fa-solid fa-circle-info"></i></a>
+        <a href="https://farmacia-atividade.vercel.app/#" className="follow button"><i className="fa-solid fa-circle-info"></i></a>
       </div>
     </div>
     
@@ -89,12 +85,7 @@ return(
       <div className="tags--overflow-container">
         <ul className="tags--container">
           <li><a href="#" className="tag">Volutpat</a></li>
-          <li><a href="#" className="tag">Scelerisque</a></li>
-          <li><a href="#" className="tag">Turpis Galo</a></li>
-          <li><a href="#" className="tag">Quisus</a></li>
-          <li><a href="#" className="tag">At Tempo</a></li>
-          <li><a href="#" className="tag">Distincto a Tet</a></li>
-          <li><a href="#" className="tag ellipsis"><i className="far fa-ellipsis-h"></i></a></li>
+        
         </ul>
       </div>
       
@@ -138,12 +129,7 @@ return(
       <div className="tags--overflow-container">
         <ul className="tags--container">
           <li><a href="#" className="tag">Gravidas</a></li>
-          <li><a href="#" className="tag">Dipisci Triste</a></li>
-          <li><a href="#" className="tag">Odio Aenean</a></li>
-          <li><a href="#" className="tag">Urna</a></li>
-          <li><a href="#" className="tag">Lista e Fortna</a></li>
-          <li><a href="#" className="tag">Vuisi</a></li>
-          <li><a href="#" className="tag ellipsis"><i className="far fa-ellipsis-h"></i></a></li>
+         
         </ul>
       </div>
       
@@ -187,11 +173,6 @@ return(
       <div className="tags--overflow-container">
         <ul className="tags--container">
           <li><a href="#" className="tag">Elementum</a></li>
-          <li><a href="#" className="tag">Praesent Semper</a></li>
-          <li><a href="#" className="tag">Gravida</a></li>
-          <li><a href="#" className="tag">Libero</a></li>
-          <li><a href="#" className="tag">Hesus</a></li>
-          <li><a href="#" className="tag ellipsis"><i className="fa-solid fa-circle-info"></i></a></li>
         </ul>
       </div>
       
