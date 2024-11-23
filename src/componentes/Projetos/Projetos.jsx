34edad14de19b5bbcc4e19b64bd3676af2e95978
yorkt1 <guilherme_oliveira18@estudante.sesisenai.org.br>
2024-11-22 21:11:31 -0300

    preparativos para componentização

--- src/componentes/Projetos/Projetos.jsx
+++ src/componentes/Projetos/Projetos.jsx
@@ -1,254 +1,277 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
+
 import './Projetos.css'
 
-function Projetos(){
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              entry.target.classList.add('showCards'); // Adiciona a classe 'show' quando visível
-            } else {
-              entry.target.classList.remove('showCards'); // Remove a classe 'show' quando não visível
-            }
-          });
-        });
-    
-        const hiddenElements = document.querySelectorAll('.hiddenCards');
-        hiddenElements.forEach((el) => observer.observe(el));
-      }, []);
-    
+function Projetos() {
 
-return(
-<div className="geralProjetos hiddenCards">
-  <div className="tituProjetos">
-    
-<h1>Meus <strong>Projetos</strong></h1>
-  </div>
-<div className="grid--container hiddenCards">
+  const [popupVisible, setPopupVisible] = useState(false);
 
+  
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('showCards'); // Adiciona a classe 'show' quando visível
+        } else {
+          entry.target.classList.remove('showCards'); // Remove a classe 'show' quando não visível
+        }
+      });
+    });
 
-<div className="grid--cell">
-  <article className="grid--item">
-    
-    <div className="preview--container">
-      <a href="https://farmacia-atividade.vercel.app/#" className="preview-imageProjetoUm--container">
-        <div className="preview-imageProjetoUm"></div>
-      </a>
-     
-    </div>
-    
-    <div className="content--container hiddenCards">
-      <div className="title--container">
-        <a className="title--text" href="https://farmacia-atividade.vercel.app/#">Projeto 1</a>
+    const hiddenElements = document.querySelectorAll('.hiddenCards');
+    hiddenElements.forEach((el) => observer.observe(el));
+  }, []);
+
+  const togglePopup = () => {
+    setPopupVisible(!popupVisible);  // Alterna a visibilidade do popup
+  };
+
+  return (
+    <div className="geralProjetos hiddenCards">
+      <div className="tituProjetos">
+
+        <h1>Meus <strong>Projetos</strong></h1>
       </div>
-      
-      <div className="tags--overflow-container" >
-      <div className="conteiner-slide">
-                <div className="paginas-slide">
-          
-                  <span>React</span>
-                  <span>HTML</span>
-                  <span>CSS</span>
-                  <span>PEROCA</span>
-                  <span>SHOTA</span>
-                  <span>React</span>
-                  <span>HTML</span>
-                  <span>CSS</span>
-                  <span>PEROCA</span>
-                  <span>SHOTA</span>
+      <div className="grid--container hiddenCards">
+
+
+        <div className="grid--cell">
+          <article className="grid--item">
+
+            <div className="preview--container">
+              <a href="https://farmacia-atividade.vercel.app/#" className="preview-imageProjetoUm--container">
+                <div className="preview-imageProjetoUm efeito"></div>
+              </a>
+
+            </div>
+
+            <div className="content--container hiddenCards">
+              <div className="title--container">
+                <a className="title--text" href="https://farmacia-atividade.vercel.app/#">landing page - Farmácia</a>
+              </div>
+
+              <div className="tags--overflow-container" >
+                <div className="conteiner-slide">
+                  <div className="paginas-slide">
+
+                    <span>React</span>
+                    <span>HTML</span>
+                    <span>CSS</span>
+                    <span>Responsividade</span>
+                    <span>React</span>
+                    <span>HTML</span>
+                    <span>CSS</span>
+                    <span>Responsividade</span>
+                    <span>React</span>
+                    <span>HTML</span>
+                  </div>
                 </div>
               </div>
-      </div>
-      
-      <div className="hover--options">
-        <a href="https://github.com/yorkt1/Farmacia-Atividade" className="Repositório button">
-          <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
-          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
-        </a>
-        
-        <a href="https://farmacia-atividade.vercel.app/#" className="Deploy
-         button">
-          <span className="icon-title"><i className="far fa-image"></i> Deploy
-        </span>
-          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
-        </a>
-        
-        <a href="https://farmacia-atividade.vercel.app/#" className="follow button"><i className="fa-solid fa-circle-info"></i></a>
-      </div>
-    </div>
-    
-  </article>
-</div>
 
-<div className="grid--cell">
-  <article className="grid--item">
-    
-    <div className="preview--container">
-      <a href="#" className="preview-image--container">
-        <div className="preview-image"></div>
-      </a>
-      
-     
-    </div>
-    
-    <div className="content--container hiddenCards">
-      <div className="title--container">
-        <a className="title--text" href="#">Projeto 2</a>
-      </div>
-      
-      <div className="tags--overflow-container">
-      <div className="conteiner-slide">
-                <div className="paginas-slide">
-          
-                  <span>React</span>
-                  <span>HTML</span>
-                  <span>CSS</span>
-                  <span>PEROCA</span>
-                  <span>SHOTA</span>
-                  <span>React</span>
-                  <span>HTML</span>
-                  <span>CSS</span>
-                  <span>PEROCA</span>
-                  <span>SHOTA</span>
+              <div className="hover--options">
+                <a href="https://github.com/yorkt1/Farmacia-Atividade" className="Repositório button">
+                  <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
+                  <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
+                </a>
+
+                <a href="https://farmacia-atividade.vercel.app/#" className="Deploy
+         button">
+                  <span className="icon-title"><i className="far fa-image"></i> Deploy
+                  </span>
+                  <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
+                </a>
+
+                <button className="follow button" onClick={togglePopup}>
+                  <i className="fa-solid fa-circle-info"></i>
+                </button>
+
+              </div>
+            </div>
+
+          </article>
+        </div>
+  {/* Popup - Modal */}
+  {popupVisible && (
+          <div className="popup-overlay">
+            <div className="popup">
+              <h2>Informações do Projeto</h2>
+              <p>Detalhes adicionais sobre o projeto...</p>
+              <button className="close-popup" onClick={togglePopup}>Fechar</button>
+            </div>
+          </div>
+        )}
+        <div className="grid--cell">
+          <article className="grid--item">
+
+            <div className="preview--container ">
+              <a href="#" className="preview-image--container">
+                <div className="preview-imageDois efeito"></div>
+              </a>
+
+
+            </div>
+
+            <div className="content--container hiddenCards">
+              <div className="title--container">
+                <a className="title--text" href="#">Projeto - Flower Experience</a>
+              </div>
+
+              <div className="tags--overflow-container">
+                <div className="conteiner-slide">
+                  <div className="paginas-slide">
+
+                    <span>React</span>
+                    <span>HTML</span>
+                    <span>CSS</span>
+                    <span>Banco De Dados</span>
+                    <span>Java</span>
+                    <span>Spring Boot</span>
+                    <span>My SQL</span>
+                    <span>React</span>
+                    <span>HTML</span>
+                    <span>CSS</span>
+                  </div>
                 </div>
               </div>
-      </div>
-      
-      <div className="hover--options">
-        <a href="#" className="Repositório button">
-          <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
-          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
-        </a>
-        
-        <a href="#" className="Deploy
+
+              <div className="hover--options">
+                <a href="https://github.com/yorkt1/FlowerExperienceProjectSA" className="Repositório button">
+                  <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
+                  <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
+                </a>
+
+                <a href="https://magoss27.github.io/DeployFlowerExperience/" className="Deploy
          button">
-          <span className="icon-title"><i className="far fa-image"></i> Deploy
-        </span>
-          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
-        </a>
-        
-        <a href="#" className="follow button"><i className="fa-solid fa-circle-info"></i></a>
-      </div>
-    </div>
-    
-  </article>
-</div>
+                  <span className="icon-title"><i className="far fa-image"></i> Deploy
+                  </span>
+                  <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
+                </a>
+
+                <button className="follow button" onClick={togglePopup}>
+                  <i className="fa-solid fa-circle-info"></i>
+                </button>
+
+              </div>
+            </div>
+
+          </article>
+        </div>
 
 
-<div className="grid--cell">
-  <article className="grid--item">
-    
-    <div className="preview--container">
-      <a href="#" className="preview-image--container">
-        <div className="preview-image"></div>
-      </a>
-      
-     
-    </div>
-    
-    <div className="content--container hiddenCards">
-      <div className="title--container">
-        <a className="title--text" href="#">Projeto 3</a>
-      </div>
-      
-      <div className="tags--overflow-container">
-      <div className="conteiner-slide">
-                <div className="paginas-slide">
-          
-                  <span>React</span>
-                  <span>HTML</span>
-                  <span>CSS</span>
-                  <span>PEROCA</span>
-                  <span>SHOTA</span>
-                  <span>React</span>
-                  <span>HTML</span>
-                  <span>CSS</span>
-                  <span>PEROCA</span>
-                  <span>SHOTA</span>
+        <div className="grid--cell">
+          <article className="grid--item">
+
+            <div className="preview--container">
+              <a href="#" className="preview-image--container">
+                <div className="preview-image"></div>
+              </a>
+
+
+            </div>
+
+            <div className="content--container hiddenCards">
+              <div className="title--container">
+                <a className="title--text" href="#">Projeto 3</a>
+              </div>
+
+              <div className="tags--overflow-container">
+                <div className="conteiner-slide">
+                  <div className="paginas-slide">
+
+                    <span>React</span>
+                    <span>HTML</span>
+                    <span>CSS</span>
+                    <span>PEROCA</span>
+                    <span>SHOTA</span>
+                    <span>React</span>
+                    <span>HTML</span>
+                    <span>CSS</span>
+                    <span>PEROCA</span>
+                    <span>SHOTA</span>
+                  </div>
                 </div>
               </div>
-      </div>
-      
-      <div className="hover--options">
-        <a href="#" className="Repositório button">
-          <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
-          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
-        </a>
-        
-        <a href="#" className="Deploy
+
+              <div className="hover--options">
+                <a href="#" className="Repositório button">
+                  <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
+                  <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
+                </a>
+
+                <a href="#" className="Deploy
          button">
-          <span className="icon-title"><i className="far fa-image"></i> Deploy
-        </span>
-          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
-        </a>
-        
-        <a href="#" className="follow button"><i className="fa-solid fa-circle-info"></i></a>
-      </div>
-    </div>
-    
-  </article>
-</div>
+                  <span className="icon-title"><i className="far fa-image"></i> Deploy
+                  </span>
+                  <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
+                </a>
+
+                <a href="#" className="follow button"><i className="fa-solid fa-circle-info"></i></a>
+              </div>
+            </div>
+
+          </article>
+        </div>
 
 
-<div className="grid--cell">
-  <article className="grid--item">
-    
-    <div className="preview--container">
-      <a href="#" className="preview-image--container">
-        <div className="preview-image"></div>
-      </a>
-      
-     
-    </div>
-    
-    <div className="content--container hiddenCards">
-      <div className="title--container">
-        <a className="title--text" href="#">Projeto 4</a>
-      </div>
-      
-      <div className="tags--overflow-container">
-      <div className="conteiner-slide">
-                <div className="paginas-slide">
-          
-                  <span>React</span>
-                  <span>HTML</span>
-                  <span>CSS</span>
-                  <span>PEROCA</span>
-                  <span>SHOTA</span>
-                  <span>React</span>
-                  <span>HTML</span>
-                  <span>CSS</span>
-                  <span>PEROCA</span>
-                  <span>SHOTA</span>
+        <div className="grid--cell">
+          <article className="grid--item">
+
+            <div className="preview--container">
+              <a href="#" className="preview-image--container">
+                <div className="preview-image"></div>
+              </a>
+
+
+            </div>
+
+            <div className="content--container hiddenCards">
+              <div className="title--container">
+                <a className="title--text" href="#">Projeto 4</a>
+              </div>
+
+              <div className="tags--overflow-container">
+                <div className="conteiner-slide">
+                  <div className="paginas-slide">
+
+                    <span>React</span>
+                    <span>HTML</span>
+                    <span>CSS</span>
+                    <span>PEROCA</span>
+                    <span>SHOTA</span>
+                    <span>React</span>
+                    <span>HTML</span>
+                    <span>CSS</span>
+                    <span>PEROCA</span>
+                    <span>SHOTA</span>
+                  </div>
                 </div>
               </div>
-      </div>
-      
-      <div className="hover--options">
-        <a href="#" className="Repositório button">
-          <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
-          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
-        </a>
-        
-        <a href="#" className="Deploy
+
+              <div className="hover--options">
+                <a href="#" className="Repositório button">
+                  <span className="icon-title"><i className="fad fa-books"></i> Repositório</span>
+                  <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
+                </a>
+
+                <a href="#" className="Deploy
          button">
-          <span className="icon-title"><i className="far fa-image"></i> Deploy
-        </span>
-          <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
-        </a>
-        
-        <a href="#" className="follow button"><i className="fa-solid fa-circle-info"></i></a>
+                  <span className="icon-title"><i className="far fa-image"></i> Deploy
+                  </span>
+                  <span className="new-tab"><i className="fas fa-arrow-circle-right"></i></span>
+                </a>
+
+                <a href="#" className="follow button"><i className="fa-solid fa-circle-info"></i></a>
+              </div>
+            </div>
+
+          </article>
+        </div>
+
+
       </div>
+
     </div>
-    
-  </article>
-</div>
-
-
-</div>
-
-           </div>
-)
+  )
 
 }
 
